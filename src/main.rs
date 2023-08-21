5eef1196857e1c8b60e7921cc3400961fe4ddc9d
MinusGix <MinusGix@gmail.com>
2023-08-21 15:18:02 -0500

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,42 @@
-use glazier::{
-    kurbo::{Point, Size},
-    AppHandle, Scale, WinHandler, WindowBuilder, WindowHandle,
-};
+use glazier::{WinHandler, WindowBuilder, WindowHandle};
 use wgpu::{Backends, InstanceDescriptor};
 
-struct Handler {
-    handle: WindowHandle,
-}
+struct Handler;
 impl WinHandler for Handler {
     fn connect(&mut self, handle: &WindowHandle) {
-        self.handle = handle.clone();
         init(handle);
     }
 
     fn prepare_paint(&mut self) {}
 
-    fn paint(&mut self, invalid: &glazier::Region) {}
+    fn paint(&mut self, _: &glazier::Region) {}
 
     fn as_any(&mut self) -> &mut dyn std::any::Any {
         self
     }
 }
 
-struct AppThing {}
+struct AppThing;
 impl glazier::AppHandler for AppThing {
-    fn command(&mut self, id: u32) {}
+    fn command(&mut self, _: u32) {}
 }
 
 pub fn init(window: &WindowHandle) {
     println!("Init");
     // Seems to default to GL
-    // let instance = wgpu::Instance::default();
+    let instance = wgpu::Instance::default();
     // Manually requiring Vulkan makes surface creation works
-    let instance = wgpu::Instance::new(InstanceDescriptor {
-        backends: Backends::VULKAN,
-        ..Default::default()
-    });
+    // let instance = wgpu::Instance::new(InstanceDescriptor {
+    //     backends: Backends::VULKAN,
+    //     ..Default::default()
+    // });
     println!("Made WGPU Instance");
 
     // Stalls if instance is gl
     let surface = unsafe { instance.create_surface(window) }.unwrap();
     println!("Made WGPU Surface");
 
-    let adapter =
+    let _adapter =
         futures::executor::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
             power_preference: wgpu::PowerPreference::default(),
             compatible_surface: Some(&surface),
@@ -56,30 +50,18 @@ pub fn init(window: &WindowHandle) {
 
 fn main() {
     env_logger::init();
+
     {
+        let handler = Box::new(Handler);
         let app = glazier::Application::new().unwrap();
-        println!("Made app");
-    }
-
-    // ...
-
-    {
-        let handler = Box::new(Handler {
-            handle: WindowHandle::default(),
-        });
-        let app = glazier::Application::global();
-        println!("Got global app");
 
         let mut builder = WindowBuilder::new(app.clone());
-        // TODO: titlebar stuff? position? size?
 
         builder = builder.handler(handler);
-        println!(" Set handler");
         let window = builder.build().unwrap();
-        println!("Made window..");
         window.show();
 
-        let app_thing = AppThing {};
+        let app_thing = AppThing;
         println!("Running");
         app.run(Some(Box::new(app_thing)));
     }
